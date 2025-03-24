ddc0c1f714cb6a4bfb28e4807b733da68ad1a117
jwchoi <jwchoi@wisenut.co.kr>
2025-03-24 15:39:12 +0900

    fix: import data from external

--- src/App.vue
+++ src/App.vue
@@ -28,6 +28,7 @@
 
 <script>
   import { colors } from './assets/colors';
+  import { data } from './assets/data/board';
 
   export default {
     name: 'App',
@@ -36,85 +37,7 @@
         colors: colors,
         isModal: false,
         modalInfo: '',
-        data: [
-          {
-            title: 'Title 1',
-            regist_time: '2025-03-24 02:07',
-            tag: ['Information', 'Top 10'],
-            content: 'This is Title 1',
-            like: 0
-          },
-          {
-            title: 'Title 2',
-            regist_time: '2025-03-24 02:15',
-            tag: ['Information', 'Humor'],
-            content: 'This is Title 2',
-            like: 0
-          },
-          {
-            title: 'Title 3',
-            regist_time: '2025-03-24 02:15',
-            tag: ['CS', 'Humor'],
-            content: 'This is Title 3',
-            like: 0
-          },
-          {
-            title: 'Title 4',
-            regist_time: '2025-03-24 02:07',
-            tag: ['Information', 'Top 10'],
-            content: 'This is Title 4',
-            like: 0
-          },
-          {
-            title: 'Title 5',
-            regist_time: '2025-03-24 02:15',
-            tag: ['Information', 'Humor'],
-            content: 'This is Title 5',
-            like: 0
-          },
-          {
-            title: 'Title 6',
-            regist_time: '2025-03-24 02:15',
-            tag: ['CS', 'Humor'],
-            content: 'This is Title 6',
-            like: 0
-          },
-          {
-            title: 'Title 7',
-            regist_time: '2025-03-24 02:15',
-            tag: ['CS', 'Humor'],
-            content: 'This is Title 7',
-            like: 0
-          },
-          {
-            title: 'Title 8',
-            regist_time: '2025-03-24 02:07',
-            tag: ['Information', 'Top 10'],
-            content: 'This is Title 8',
-            like: 0
-          },
-          {
-            title: 'Title 9',
-            regist_time: '2025-03-24 02:15',
-            tag: ['Information', 'Humor'],
-            content: 'This is Title 9',
-            like: 0
-          },
-          {
-            title: 'Title 10',
-            regist_time: '2025-03-24 02:15',
-            tag: ['CS', 'Humor'],
-            content: 'This is Title 10',
-            like: 0
-          },
-          {
-            title: 'Title 11',
-            regist_time: '2025-03-24 02:15',
-            tag: ['CS', 'CV'],
-            content: 'This is Title 11',
-            like: 0
-          }
-        ]
+        data: data
       }
     },
     methods: {
