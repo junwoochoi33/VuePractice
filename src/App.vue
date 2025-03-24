fdcdf2f921665d61017a9d1f4cb7c29c1c464531
jwchoi <jwchoi@wisenut.co.kr>
2025-03-24 14:34:36 +0900

    feat: add like button

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <template>
-  <h1>게시판</h1>
+  <h1>Bulletin board</h1>
 
   <div v-for="(item, i) in data" :key="i">
     <h3 class="bg-gold pad-title" :style="item.title_text_color">{{ item.title }}</h3>
-    <p>등록일시: {{ item.regist_time }}</p>
-    <p>태그: {{ item.tag.join(', ') }}</p> 
+    <p>Registed datetime: {{ item.regist_time }}</p>
+    <p>Tags: {{ item.tag.join(', ') }}</p> 
+    <button @:click="item.like++"><span>Like: {{ item.like }}</span></button>
   </div>
 
 </template>
@@ -16,22 +17,25 @@
       return {
         data: [
           {
-            title: '제목 1',
+            title: 'Title 1',
             title_text_color: "color: white",
             regist_time: '2025-03-24 02:07',
-            tag: ['정보', '인기'],
+            tag: ['Information', 'Top 10'],
+            like: 0
           },
           {
-            title: '제목 2',
+            title: 'Title 2',
             title_text_color: "color: black",
             regist_time: '2025-03-24 02:15',
-            tag: ['정보', '유머'],
+            tag: ['Information', 'Humor'],
+            like: 0
           },
           {
-            title: '제목 3',
+            title: 'Title 3',
             title_text_color: "color: purple",
             regist_time: '2025-03-24 02:15',
-            tag: ['심리', '유머'],
+            tag: ['CS', 'Humor'],
+            like: 0
           }
         ]
       }
